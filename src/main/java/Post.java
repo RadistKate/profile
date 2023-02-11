a1b59b062a81af814492cfdaf824f440eb472472
Katy test <artebyakinae@mail.ru>
2023-02-11 18:11:26 +0300

    Inisial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+
+    public String phone; // поле для телефона
+
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле согласия об оповещении по смс
+    public FormDate birthday ; // поле для даты рождения
+}
